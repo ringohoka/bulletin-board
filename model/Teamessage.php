3bb38dc82e5967ef400a185e9768bc924ca7f379
soranin08 <processpurchasesat@gmail.com>
2022-06-24 10:26:30 +0900

    Create Teamessage.php

---
+++ model/Teamessage.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Teamessage extends Model
+{
+    use HasFactory;
+
+    use SoftDeletes;
+
+    protected $table = 'Teachers';
+    protected $primaryKey = 'TNO';
+
+    public function thumbnails(){
+        return $this -> hasOne('App\Models\Thumbnail');
+    }
+}
