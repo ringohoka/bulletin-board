1e46da26dcbb75438c2b8cbebb14e5dc12e7c089
soranin08 <processpurchasesat@gmail.com>
2022-07-08 09:50:15 +0900

    Update srudent_login.blade.php

--- srudent_login.blade.php
+++ srudent_login.blade.php
@@ -14,15 +14,24 @@
     <form action="login.php" method="post">
     <div>
         <label>学籍番号:<label>
-        <input type="text" name="namber" required>
+        <input type="text" name="number" required>
+        @error('number')
+            <p class="text-red-500">{{$message}}</p>
+        @enderror
     </div>
     <div>
         <label>名前:<label>
         <input type="text" name="name" required>
+        @error('name')
+            <p class="text-red-500">{{$message}}</p>
+        @enderror
     </div>
     <div>
         <label>パスワード：<label>
         <input type="password" name="pass" required>
+        @error('pass')
+            <p class="text-red-500">{{$message}}</p>
+        @enderror
     </div>
     <input type="submit" value="ログイン">
     </form>
